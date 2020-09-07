955517354260eb4fdf576dc6ce4d1f36e9e46ed5
liuxuan00 <33903479@qq.com>
2020-09-07 19:53:39 +0800

    main

--- app/src/main/java/com/example/toutiaonews/MainActivity.java
+++ app/src/main/java/com/example/toutiaonews/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.toutiaonews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("xxx", "onCreate: "+"嘟嘟嘟 ");
-        Log.e("xxx", "onCreate: "+"任狗飞 ");
-        Log.e("xxx", "onCreate: "+"嘟嘟嘟 ");
-        Log.e("xxx", "onCreate: "+"嘟嘟嘟 ");
+
+
     }
 }
