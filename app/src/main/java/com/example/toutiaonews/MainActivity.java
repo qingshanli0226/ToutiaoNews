e48bbd64b0f93c9b0d3756b04567d1a23fd30b23
jialeliu <1005400916@qq.com>
2020-09-07 17:20:55 +0800

    ljl0907

--- app/src/main/java/com/example/toutiaonews/MainActivity.java
+++ app/src/main/java/com/example/toutiaonews/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.toutiaonews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.chaychan.library.BottomBarLayout;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomBarLayout bbl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("hq", "onCreate: " );
-        Toast.makeText(this, "傻逼胡强", Toast.LENGTH_SHORT).show();
+
+        initView();
+        initData();
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        bbl = findViewById(R.id.bbl);
+
     }
 }
