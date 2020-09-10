e274b450b15165529a5d2b39dcbe1f35d51cf004
zhaochenxu0719 <2365134432@qq.com>
2020-09-10 10:29:50 +0800

    9.10first

--- Videolibrary/src/main/java/com/example/videolibrary/MainActivity.java
+++ Videolibrary/src/main/java/com/example/videolibrary/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.shuyu.gsyvideoplayer.GSYVideoManager;
+import com.shuyu.gsyvideoplayer.listener.GSYVideoProgressListener;
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private OrientationUtils orientationUtils;
     private String url="http://ips.ifeng.com/video19.ifeng.com/video09/2014/06/16/1989823-102-086-0009.mp4";
     private String title="节目";
+    private long time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         gsyplayer = (StandardGSYVideoPlayer) findViewById(R.id.gsyplayer);
     }
 
+
     private void initData() {
         gsyplayer.setUp(url, false, title);
         //设置旋转
@@ -42,10 +46,12 @@ public class MainActivity extends AppCompatActivity {
         gsyplayer.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+//                time = gsyplayer.getGSYVideoManager().getDuration();
+//                gsyplayer.getGSYVideoManager().seekTo(time);
                 orientationUtils.resolveByClick();  //此方法是切换屏幕旋转，例如现在是竖屏，调用后变横屏，反正一样，设置这个之前，先把Activity禁止横竖屏切换，并且竖屏模式，不然此方法无
             }
         });
+
         gsyplayer.getBackButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
