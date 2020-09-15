f16524fa203cf03edbd31c0dd0af14db3c3a6db4
hmx1999 <805973197@qq.com>
2020-09-15 16:53:30 +0800

    更新错误页面

---
+++ net/src/main/java/com/example/net/obserable/NetBusinessException.java
@@ -0,0 +1,29 @@
+package com.example.net.obserable;
+
+//定义一个异常类，该异常类封装服务端返回的业务错误
+public class NetBusinessException extends Exception {
+    private String errorCode;
+    private String errorMessage;
+
+    public NetBusinessException(String code, String messsage) {
+        super(messsage);
+        this.setErrorCode(code);
+        this.setErrorMessage(messsage);
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
