ac7130e7361cf0ac225fade063ce0050689aaf06
84wangmou <2247184831@qq.com>
2020-09-15 10:03:15 +0800

    homemodule

--- homemodule/src/main/java/com/bw/homemodule/HomeFragment.java
+++ homemodule/src/main/java/com/bw/homemodule/HomeFragment.java
@@ -1,5 +1,9 @@
 package com.bw.homemodule;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -10,23 +14,24 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.ArrayList;
 
 
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements View.OnClickListener {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ColorTabLayoutAdapter tabLayoutAdapter;
     private ArrayList<Fragment> fragments = new ArrayList<>();
     private String[] channel;
-
+    private ImageView homeAdd;
 
     @Override
     protected int bandLayout() {
         return R.layout.fragment_home;
-
     }
 
     @Override
     protected void initData() {
         fragments.clear();
+        channel = null;
+
         channel = getResources().getStringArray(R.array.channel);
 
         for (String str : channel) {
@@ -37,13 +42,17 @@ public class HomeFragment extends BaseFragment {
         viewPager.setAdapter(tabLayoutAdapter);
         viewPager.setOffscreenPageLimit(channel.length);
         tabLayout.setupWithViewPager(viewPager);
+
+
     }
 
     @Override
     protected void initView() {
         viewPager = findViewById(R.id.home_fragment_viewpager);
         tabLayout = findViewById(R.id.home_fragment_colorTab);
+        homeAdd = findViewById(R.id.home_fragment_topAdd);
 
+        homeAdd.setOnClickListener(this);
     }
 
     @Override
@@ -51,5 +60,14 @@ public class HomeFragment extends BaseFragment {
         super.onFragmentVisibleChange(isVisible);
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.home_fragment_topAdd:
+                Toast.makeText(getActivity(), "add", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
---
+++ homemodule/src/main/java/com/bw/homemodule/ResetTopActivity.java
@@ -0,0 +1,14 @@
+package com.bw.homemodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetTopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_top);
+    }
+}
