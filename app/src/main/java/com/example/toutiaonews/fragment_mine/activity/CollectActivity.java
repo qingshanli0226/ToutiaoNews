181e25a0c0868fa4c75717973d8d3c2da36f37f7
jialeliu <1005400916@qq.com>
2020-09-15 19:45:01 +0800

    mine newss

--- app/src/main/java/com/example/toutiaonews/fragment_mine/activity/CollectActivity.java
+++ app/src/main/java/com/example/toutiaonews/fragment_mine/activity/CollectActivity.java
@@ -1,16 +1,29 @@
 package com.example.toutiaonews.fragment_mine.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
-import android.os.Bundle;
-
+import com.example.framework2.mvp.view.BaseActivity;
 import com.example.toutiaonews.R;
 
-public class CollectActivity extends AppCompatActivity {
+public class CollectActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_collect);
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public int bandLayout() {
+        return R.layout.activity_collect;
     }
 }
